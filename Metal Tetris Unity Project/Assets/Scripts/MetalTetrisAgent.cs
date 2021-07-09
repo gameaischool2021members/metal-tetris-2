6b7b9cb10edcd5a46d1df88a6eed1e04731994af
Prabhav Bhatnagar <prabhav2b@gmail.com>
2021-07-09 08:18:05 +0300

    more agent setup

--- Metal Tetris Unity Project/Assets/Scripts/MetalTetrisAgent.cs
+++ Metal Tetris Unity Project/Assets/Scripts/MetalTetrisAgent.cs
@@ -10,15 +10,16 @@ public class MetalTetrisAgent : Agent
     [SerializeField, Range(0.01f, 1f)] float moveTime = 0.1f;
     
     private DeliverySystem deliverySystem;
-    //Match3Board Board;
+    private AgentWorldStateGetter m_AgentWorldStateGetter;
     
     private float m_TimeUntilMove;
     private int m_MovesMade;
+    private int rowsFilled;
     
     void Start()
     {
         deliverySystem = FindObjectOfType<DeliverySystem>();
-        
+        m_AgentWorldStateGetter = FindObjectOfType<AgentWorldStateGetter>();
     }
 
     public override void OnEpisodeBegin()
@@ -27,6 +28,7 @@ public class MetalTetrisAgent : Agent
         
         deliverySystem.ClearGrid();
         m_TimeUntilMove = moveTime;
+        rowsFilled = 0;
         m_MovesMade = 0;
         //Reset the Agent when needed
 
@@ -35,7 +37,14 @@ public class MetalTetrisAgent : Agent
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        //do nothing
+        int[,] gridStatus = m_AgentWorldStateGetter.GridStatus(); //6 x 10
+        foreach (var val in gridStatus)
+        {
+            sensor.AddObservation(val);
+        }
+        
+        sensor.AddObservation(m_AgentWorldStateGetter.PorcentageFilled());
+        sensor.AddObservation(m_AgentWorldStateGetter.RowsCompletelyOccupied());
     }
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
@@ -44,7 +53,14 @@ public class MetalTetrisAgent : Agent
 
 
         // Rewards
+        AddReward(m_AgentWorldStateGetter.PorcentageFilled());
 
+        int newRowsFilled = m_AgentWorldStateGetter.RowsCompletelyOccupied();
+        if(newRowsFilled > rowsFilled)
+        {
+            rowsFilled = newRowsFilled;
+            AddReward(rowsFilled);
+        }
 
         // Reached target
 
